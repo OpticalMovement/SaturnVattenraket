1e073de7dab66d8cc21874bd66295d777407dadd
Jonathan Myhr <OpticalMovement@github.com>
2020-12-01 12:39:28 +0100

    Update

---
+++ MassanVattenUt.m
@@ -0,0 +1,8 @@
+function [MwT] = MassanVattenUt(Pw, Vwn, S)
+% Returnerar hastigheten för vattnets utflöde
+
+MwT = -Pw*Vwn*S;
+
+
+end
+
---
+++ RaketensHastighet.m
@@ -0,0 +1,13 @@
+function [VbT] = RaketensHastighet(mb,ma)
+
+f = 
+
+Mb = 0.1;
+Ma = 0.001;
+
+
+
+VbT = -(1/(Mb+Ma))*(Fd+Fm+Wx)+(1/(Mb+Ma)
+
+end
+
